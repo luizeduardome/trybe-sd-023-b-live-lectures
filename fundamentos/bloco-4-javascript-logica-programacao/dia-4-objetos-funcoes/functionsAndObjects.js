11e546cd2a073ba6cc7cabb2e8bb1f10275dc062
Luiz Eduardo <airmaesterdev@outlook.com>
2022-05-17 16:54:52 -0300

    Criado as funções com 1 e 2 parâmetros.

--- fundamentos/bloco-4-javascript-logica-programacao/dia-4-objetos-funcoes/functionsAndObjects.js
+++ fundamentos/bloco-4-javascript-logica-programacao/dia-4-objetos-funcoes/functionsAndObjects.js
@@ -2,15 +2,33 @@
 
 // Função morningGreeting
 
-function morningGreeting() {
-    console.log('Bom dia!');
-}
+// function morningGreeting() {
+//     return 'Bom dia!';
+// }
 
-morningGreeting();
+// console.log(morningGreeting());
+// let texto = morningGreeting();
+// console.log(texto);
+
+
 // Função sem return
 
 // Função com return
 
 // Função com parâmetro
 
+// function morningGreeting(name) {
+//     return 'Bom dia, ' + name;
+// }
+
+// let nome = 'Luiz';
+// console.log(morningGreeting(nome));
+
 // Função soma - dois parâmetros
+
+function sum(number1, number2) {
+    console.log('number2:', number2);
+    return number1 + number2;
+}
+
+console.log(sum(3, 5));
