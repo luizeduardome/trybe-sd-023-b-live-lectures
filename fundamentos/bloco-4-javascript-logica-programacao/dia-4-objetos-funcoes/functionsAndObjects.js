bfeabf279ee948e99781e94a40d195a5968522d2
Luiz Eduardo <airmaesterdev@outlook.com>
2022-05-17 16:40:21 -0300

    Criado a função 'morningGreeting()'.

---
+++ fundamentos/bloco-4-javascript-logica-programacao/dia-4-objetos-funcoes/functionsAndObjects.js
@@ -0,0 +1,16 @@
+// Parte 1 - Funções
+
+// Função morningGreeting
+
+function morningGreeting() {
+    console.log('Bom dia!');
+}
+
+morningGreeting();
+// Função sem return
+
+// Função com return
+
+// Função com parâmetro
+
+// Função soma - dois parâmetros
